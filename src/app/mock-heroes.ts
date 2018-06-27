40642300560ac0ecae60892a04cfe5332b1d0836
Thinbk <nguyendangthin789@gmail.com>
2018-06-27 15:16:26 +0700

    first commiit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+
+import {Hero} from './heroes/hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name : 'hi'},
+  {id: 12, name : 'He'},
+  {id: 13, name : 'Ha'},
+  {id: 14, name : 'Trang'},
+  {id: 15, name : 'Anh'},
+  {id: 16, name : 'Em'},
+  {id: 17, name : 'Thin'},
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+
+]
