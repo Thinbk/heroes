31f8bc045d8583eb8885fa44133e233fbe8e071b
Thinbk <nguyendangthin789@gmail.com>
2018-06-27 17:48:06 +0700

    version2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 
 import { Component, OnInit } from '@angular/core';
-
 import {Hero} from './hero';
 import {HeroService} from '../hero.service';
 
@@ -10,7 +9,6 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
 
   heroes: Hero[];
 
@@ -18,10 +16,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
